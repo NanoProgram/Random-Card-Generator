e33369c581574971968c77f115a3ec0689db2f8c
NanoProgram <danielj.perezd@gmail.com>
2022-11-21 21:39:09 +0000

    Finalizado

--- src/app.js
+++ src/app.js
@@ -15,10 +15,6 @@ function numAleatorio() {
 
 function simAleatorio() {
   let aleatorioSim = Math.floor(Math.random() * 4);
-  /*if (aleatorioSim === 3 || aleatorioSim === 2) {
-    document.getElementById("up").style.color = "#CC0000";
-    document.getElementById("down").style.color = "#CC0000";
-  }*/
   return aleatorioSim;
 }
 
@@ -26,28 +22,22 @@ setInterval(function() {
   console.log("Hola");
 }, 1000);
 
-/*window.onload = function change() {
-  let sim = simAleatorio();
-  document.getElementById("up").innerHTML = simbolos[sim];
-  document.getElementById("down").innerHTML = simbolos[sim];
-  let num = numAleatorio();
-  document.getElementById("center").innerHTML = numeros[num];
-  if (sim === 2 || sim === 3) {
-    document.getElementById("center").style.color = "#CC0000";
-  }
-};*/
-
-window.onload = function change() {
-  let sim = simAleatorio();
-  document.getElementById("up").innerHTML = simbolos[sim];
-  document.getElementById("down").innerHTML = simbolos[sim];
-  let num = numAleatorio();
-  document.getElementById("center").innerHTML = numeros[num];
-  if (sim === 2 || sim === 3) {
-    document.getElementById("center").style.color = "#CC0000";
-  }
-  if (sim === 3 || sim === 2) {
-    document.getElementById("up").style.color = "#CC0000";
-    document.getElementById("down").style.color = "#CC0000";
-  }
-};
+setInterval(
+  (window.onload = function change() {
+    let sim = simAleatorio();
+    document.getElementById("up").innerHTML = simbolos[sim];
+    document.getElementById("down").innerHTML = simbolos[sim];
+    let num = numAleatorio();
+    document.getElementById("center").innerHTML = numeros[num];
+    if (sim === 2 || sim === 3) {
+      document.getElementById("center").style.color = "#CC0000";
+      document.getElementById("up").style.color = "#CC0000";
+      document.getElementById("down").style.color = "#CC0000";
+    } else {
+      document.getElementById("center").style.color = "#000";
+      document.getElementById("up").style.color = "#000";
+      document.getElementById("down").style.color = "#000";
+    }
+  }),
+  1000
+);
